33e5abe41a1abc3273709ecf3e01c4d51da7bc10
Ray Luna <leon.luna.ray@gmail.com>
2023-04-08 19:30:08 -0700

    add contact details and intro bio

--- js/sanity.js
+++ js/sanity.js
@@ -11,3 +11,8 @@ export async function getFeaturedProjects() {
   const projects = await client.fetch('*[_type == "project" && featured] | order(_updatedAt desc)')
   return projects
 }
+
+export async function getProfile() {
+  const profile = await client.fetch('*[_type == "profileDetails"][0]')
+  return profile
+}
