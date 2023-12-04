4f1a3a1a06869cd95d0238ce83dedfa85367fa93
Ray Luna <leon.luna.ray@gmail.com>
2023-12-03 17:45:10 -0800

    add page routing, dark mode

---
+++ js/darkMode.js
@@ -0,0 +1,24 @@
+const html = document.querySelector('html');
+const darkModeBtn = document.getElementById('dark-mode-btn');
+const darkPreference = window.matchMedia('(prefers-color-scheme: dark)');
+const isDarkMode = html.classList.contains('dark');
+
+// Methods
+const toggleDarkMode = () => {
+  html.classList.toggle('dark');
+};
+const handleDarkModeChange = () => {
+  if ((!isDarkMode && darkPreference.matches) || (isDarkMode && !darkPreference.matches)) {
+    toggleDarkMode();
+  }
+};
+
+// Events
+darkPreference.addEventListener('change', handleDarkModeChange);
+
+// darkModeBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   toggleDarkMode();
+// });
+
+handleDarkModeChange();
